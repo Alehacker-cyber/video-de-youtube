596d0b90774a9ab28908eb87608a46113db85ec0
Alehacker-cyber <beatonprohenzaa@gmail.com>
2025-08-12 00:25:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/<filename>")
-def serve_audio(filename):
-    return send_from_directory(".", filename)
